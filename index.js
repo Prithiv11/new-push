7fc7e042fcead67bfa24667e7fe2b5d5fa352955
Prithiv11 <prithiviraj624@gmail.com>
2022-04-21 11:44:08 +0530

    token updated

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ async function createConnection() {
 export const client = await createConnection();
 
 app.get("/", function (req, res) {
-  res.send("Hello World");
+  res.send("Hello Everyone");
 });
 app.use("/movies", moviesRouter);
 app.use("/users", usersRouter);
